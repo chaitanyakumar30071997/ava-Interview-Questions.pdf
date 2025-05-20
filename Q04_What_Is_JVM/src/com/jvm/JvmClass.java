40a88ecc6da561b51c8edb7e7fbafb864fe0b25e
chaitanyakumar3009 <chaitanyakumar3009@gmail.com>
2025-05-20 13:34:44 +0530

    Q4. What is JVM?

---
+++ Q04_What_Is_JVM/src/com/jvm/JvmClass.java
@@ -0,0 +1,14 @@
+package com.jvm;
+
+/*  Q4. What is JVM? 
+The Java Virtual Machine (JVM) is a part of the Java Runtime Environment 
+(JRE). It is responsible for executing Java bytecode by converting it into 
+machine code specific to the operating system.*/
+
+public class JvmClass {
+	
+	public static void main(String[] args) {
+        System.out.println("Hello, JVM!");
+    }
+
+}

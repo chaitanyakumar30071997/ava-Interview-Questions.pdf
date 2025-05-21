f44678fca9c62d723019c676c5bf562f8e1b61fc
chaitanyakumar3009 <chaitanyakumar3009@gmail.com>
2025-05-21 16:46:46 +0530

    Q24. What is a package in Java ?

---
+++ Q24_Package/src/Mypackage/MyClass.java
@@ -0,0 +1,15 @@
+package Mypackage;
+
+/*Q24. What is a package in Java? 
+A package in Java is a collection of related classes and interfaces grouped to 
+organize code and prevent naming conflicts. 
+ ● Built-in packages: java.lang, java.util, etc.
+ ● User-defined packages: Created by developers for organizing custom
+ classes. */
+
+public class MyClass {
+
+	public void display() {
+        System.out.println("Hello from MyClass in mypackage!");
+    }
+}

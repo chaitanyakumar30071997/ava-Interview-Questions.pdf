ef0c2234ac04aeef34df3b149b4b04b62edc11d7
chaitanyakumar3009 <chaitanyakumar3009@gmail.com>
2025-05-21 16:31:49 +0530

    Q15. What do you mean by the final keyword ?

---
+++ Q15_final_keyword/src/com/Final_keyword/FinalClass.java
@@ -0,0 +1,34 @@
+package com.Final_keyword;
+
+/* Q15. What do you mean by the final keyword? 
+ * The final keyword is used to restrict modifications in Java. It can be applied 
+   in three contexts: 
+ 1. Final variable: Its value cannot be changed once assigned.
+ 2. Final method: Prevents method overriding in subclasses.
+ 3. Final class: Prevents inheritance by other classes
+ *  */
+
+// Final class cannot be inherited
+public  final class FinalClass {
+	
+	// final variable
+	int var = 10;
+	
+	// final method
+	void display() {
+        System.out.println("This is a final class");
+    }
+    //** In final class, constructor cannot be final **
+	// final constructor
+	// Constructor cannot be final
+	// Constructor cannot be static
+	// Constructor cannot be private
+	// Constructor cannot be abstract
+	// Constructor cannot be native
+	// Constructor cannot be synchronized
+	// Constructor cannot be strictfp
+	// In final class, constructor cannot be final
+	FinalClass() {
+        System.out.println("Constructor of final class");
+    }
+}

4107091e9ac3643199141a4ed04a9a9eb0486afc
chaitanyakumar3009 <chaitanyakumar3009@gmail.com>
2025-05-22 14:40:40 +0530

    Q31. What is the difference between JDK, JRE, and JVM ?

---
+++ Q31_JDK_JRE_JVM/src/com/jdk_jre/OneClasss.java
@@ -0,0 +1,17 @@
+package com.jdk_jre;
+
+public class OneClasss {
+	
+	/*
+Q31. What is the difference between JDK, JRE, and JVM? 
+ ● JDK (Java Development Kit) – Contains JRE + development tools
+ (compiler, debugger) for developing and running Java applications.
+ ● JRE (Java Runtime Environment) – Includes JVM + libraries needed
+ to run Java applications but lacks development tools.
+ ● JVM (Java Virtual Machine) – Executes Java bytecode, providing
+ platform independence and memory management (GC).
+ JDK > JRE > JVM – JDK includes JRE, and JRE includes JVM. 
+JDK is for developers, while JRE is for users running Java applications
+	 */
+
+}
